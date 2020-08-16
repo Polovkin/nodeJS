1ae54c415a4cfba5189f4612573a5352a87b14b6
Polovkin <zimboroda@gmail.com>
2020-08-16 20:15:03 +0300

    123

--- index.js
+++ index.js
@@ -1,67 +1,28 @@
-const http = require('http');
-const path = require('path');
-const fs = require('fs');
-
-const server = http.createServer((req, res) => {
-    if (req.method === 'GET') {
-        res.writeHead(200, {
-            'Content-Type': 'text/html; charset=utf-8'
-        })
-
-        if (req.url === '/') {
-            fs.readFile(
-                path.join(__dirname, 'views', 'index.html'),
-                'utf-8',
-                (err, content) => {
-                    if (err) {
-                        throw err
-                    }
-
-                    res.end(content)
-                }
-            )
-        } else if (req.url === '/about') {
-            fs.readFile(
-                path.join(__dirname, 'views', 'about.html'),
-                'utf-8',
-                (err, content) => {
-                    if (err) {
-                        throw err
-                    }
-
-                    res.end(content)
-                }
-            )
-        } else if (req.url === '/api/users') {
-            res.writeHead(200, {
-                'Content-type': 'text/json'
-            })
-            const users = [
-                {name: 'Mikle', age: 29},
-                {name: 'Valera', age: 33}
-            ]
-            res.end(JSON.stringify(users))
-        }
-
-    } else if (req.method === 'POST') {
-        const body = []
-        res.writeHead(200, {
-            'Content-type': 'text/html; charset=utf-8'
-        })
-        req.on('data', data => {
-            body.push(Buffer.from(data))
-        })
-
-        req.on('end', () => {
-            const message = body.toString().split('=')[1];
-            res.end(`
-                <h1>Ваше сообщение: ${message}</h1>    
-                `)
-        })
-
-    }
+const express = require('express');
+const exphbr = require('express-handlebars')
+const app = express();
+const homeRoutes = require('./routes/home-page')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
+const hbs = exphbr.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
 })
 
-server.listen(3000, () => {
-    console.log('Server is running...')
+app.engine('hbs',hbs.engine);
+app.set('view engine','hbs')
+app.set('views','views')
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use('/',homeRoutes)
+app.use('/add',addRoutes)
+app.use('/courses',coursesRoutes)
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
