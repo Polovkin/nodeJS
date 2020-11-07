b093d27b6d1a41c158bc36d0da3c54d34a9e8093
Polovkin <zimboroda@gmail.com>
2020-11-07 12:57:06 +0200

    32

--- routes/card.js
+++ routes/card.js
@@ -4,19 +4,23 @@ const Course = require('../models/course')
 const router = Router()
 
 router.post('/add', async (req, res) => {
-  const course = await Course.getById(req.body.id)
-  await Card.add(course)
-  res.redirect('/card')
+    const course = await Course.getById(req.body.id)
+    await Card.add(course)
+    res.redirect('/card')
 })
 
 router.get('/', async (req, res) => {
-  const card = await Card.fetch()
-  res.render('card', {
-    title: 'Корзина',
-    isCard: true,
-    courses: card.courses,
-    price: card.price
-  })
+    const card = await Card.fetch()
+    res.render('card', {
+        title: 'Корзина',
+        isCard: true,
+        courses: card.courses,
+        price: card.price,
+    })
+})
+router.delete(`/remove/:id`, async (res, req) => {
+    const card = await Card.remove(req.params.id)
+    await res.status(200).json(card)
 })
 
 module.exports = router
