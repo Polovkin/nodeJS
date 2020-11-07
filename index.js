0e22a97785508d6959cf8030bac9bc76681d0418
Polovkin <zimboroda@gmail.com>
2020-11-07 16:11:15 +0200

    fail

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ const coursesRoutes = require('./routes/courses')
 const app = express()
 
 const hbs = exphbs.create({
-    defaultLayout: 'main',
-    extname: 'hbs'
+  defaultLayout: 'main',
+  extname: 'hbs'
 })
 
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 
 app.use('/', homeRoutes)
@@ -26,7 +26,7 @@ app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 async function start() {
     try {
--- routes/courses.js
+++ routes/courses.js
@@ -3,7 +3,8 @@ const Course = require('../models/course')
 const router = Router()
 
 router.get('/', async (req, res) => {
-    const courses = await Course.find()
+
+    const courses = await Course.find().lean()
     res.render('courses', {
         title: 'Курсы',
         isCourses: true,
@@ -12,6 +13,7 @@ router.get('/', async (req, res) => {
 })
 
 router.get('/:id/edit', async (req, res) => {
+
     if (!req.query.allow) {
         return res.redirect('/')
     }
@@ -25,13 +27,14 @@ router.get('/:id/edit', async (req, res) => {
 })
 
 router.post('/edit', async (req, res) => {
-    const {id} = req.body;
+    const {id} = req.body
     delete req.body.id
     await Course.findByIdAndUpdate(id, req.body)
     res.redirect('/courses')
 })
 
 router.get('/:id', async (req, res) => {
+
     const course = await Course.findById(req.params.id)
     res.render('course', {
         layout: 'empty',
