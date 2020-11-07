4f3ff0ad64369ce6ab372ac3f7794b4966cea2d5
Polovkin <zimboroda@gmail.com>
2020-11-07 12:20:02 +0200

    32

--- routes/courses.js
+++ routes/courses.js
@@ -3,36 +3,39 @@ const Course = require('../models/course')
 const router = Router()
 
 router.get('/', async (req, res) => {
-    const courses = await Course.getAll()
-    res.render('courses', {
-        title: 'Курсы',
-        isCourses: true,
-        courses
-    })
+  const courses = await Course.getAll()
+  res.render('courses', {
+    title: 'Курсы',
+    isCourses: true,
+    courses
+  })
 })
 
 router.get('/:id/edit', async (req, res) => {
-    if (!req.query.allow) {
-        return res.redirect('/')
-    }
-    const course = await Course.getById(req.params.id)
+  if (!req.query.allow) {
+    return res.redirect('/')
+  }
 
-    res.render('course-edit', {
-        title: `Редактировать ${course.title}`,
-        course
-    })
+  const course = await Course.getById(req.params.id)
+
+  res.render('course-edit', {
+    title: `Редактировать ${course.title}`,
+    course
+  })
 })
 
-router.post('/edit',async (req,res) => {
-    await Course.update(req.body);
-    res.redirect('/courses')
+router.post('/edit', async (req, res) => {
+  await Course.update(req.body)
+  res.redirect('/courses')
 })
+
 router.get('/:id', async (req, res) => {
-    const course = await Course.getById(req.params.id)
-    res.render('course', {
-        layout: 'empty',
-        title: `Курсы ${course.title}`,
-        course
-    })
+  const course = await Course.getById(req.params.id)
+  res.render('course', {
+    layout: 'empty',
+    title: `Курс ${course.title}`,
+    course
+  })
 })
+
 module.exports = router
