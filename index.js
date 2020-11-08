020d53f30e5a4f5aab3e4ae71b0dd3c8015a8591
Polovkin <zimboroda@gmail.com>
2020-11-08 20:52:21 +0200

    42 done

--- index.js
+++ index.js
@@ -7,16 +7,27 @@ const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
 const User = require('./models/user')
+
 const app = express()
 
 const hbs = exphbs.create({
-    defaultLayout: 'main',
-    extname: 'hbs'
+  defaultLayout: 'main',
+  extname: 'hbs'
 })
 
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
+
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('5cc1d29dcedab01481e03660')
+    req.user = user
+    next()
+  } catch (e) {
+    console.log(e)
+  }
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
@@ -26,40 +37,30 @@ app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
 
-app.use(async (req, res, next) => {
-    try {
-        const user = await User.findById('5fa8332ea1e05d1138436b27')
-        req.user = user
-        next()
-    } catch (e) {
-        console.log(e);
-        throw e
-    }
-
-})
 const PORT = process.env.PORT || 3000
 
 async function start() {
-    try {
-        const url = `mongodb+srv://polovakin_1488:KTvv5SMazgP48ff@cluster0.kgzyb.mongodb.net/shop`
-        await mongoose.connect(url, {useNewUrlParser: true, useFindAndModify: false})
-
-        const candidate = await User.findOne()
-        if (!candidate) {
-            const user = new User({
-                email: 'mail@mail.ru',
-                name: 'Mikle',
-                cart: {items: []}
-            })
-            await user.save()
-        }
-
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`)
-        })
-    } catch (e) {
-        console.log(e)
+  try {
+    const url = `mongodb+srv://vladilen:0I5GEL9uLUcR38GC@cluster0-3rrau.mongodb.net/shop`
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useFindAndModify: false
+    })
+    const candidate = await User.findOne()
+    if (!candidate) {
+      const user = new User({
+        email: 'vladilen@mail.ru',
+        name: 'Vladilen',
+        cart: {items: []}
+      })
+      await user.save()
     }
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 start()
