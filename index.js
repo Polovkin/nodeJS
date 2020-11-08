77a10b06dec2328d2f4e445d2fdcfd5754167945
Polovkin <zimboroda@gmail.com>
2020-11-08 20:42:43 +0200

    39 done

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
-
+const User = require('./models/user')
 const app = express()
 
 const hbs = exphbs.create({
-  defaultLayout: 'main',
-  extname: 'hbs'
+    defaultLayout: 'main',
+    extname: 'hbs'
 })
 
 app.engine('hbs', hbs.engine)
@@ -26,18 +26,40 @@ app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
 
+app.use(async (req, res, next) => {
+    try {
+        const user = await User.findById('5fa8332ea1e05d1138436b27')
+        req.user = user
+        next()
+    } catch (e) {
+        console.log(e);
+        throw e
+    }
+
+})
 const PORT = process.env.PORT || 3000
 
 async function start() {
-  try {
-    const url = `mongodb+srv://polovakin_1488:KTvv5SMazgP48ff@cluster0.kgzyb.mongodb.net/shop`
-    await mongoose.connect(url, {useNewUrlParser: true})
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`)
-    })
-  } catch (e) {
-    console.log(e)
-  }
+    try {
+        const url = `mongodb+srv://polovakin_1488:KTvv5SMazgP48ff@cluster0.kgzyb.mongodb.net/shop`
+        await mongoose.connect(url, {useNewUrlParser: true, useFindAndModify: false})
+
+        const candidate = await User.findOne()
+        if (!candidate) {
+            const user = new User({
+                email: 'mail@mail.ru',
+                name: 'Mikle',
+                cart: {items: []}
+            })
+            await user.save()
+        }
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    } catch (e) {
+        console.log(e)
+    }
 }
 
 start()
