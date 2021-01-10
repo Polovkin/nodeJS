f68f65799bc1c7e05ce09f6842f0f1870bb5b2c8
Polovkin <zimboroda@gmail.com>
2021-01-10 13:56:14 +0200

    60

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,9 @@ const router = Router()
 router.get('/login', async (req, res) => {
     res.render('auth/login', {
         title: 'Авторизация',
-        isLogin: true
+        isLogin: true,
+        loginError: req.flash('loginError'),
+        registerError: req.flash('registerError')
     })
 })
 
@@ -32,9 +34,11 @@ router.post('/login', async (req, res) => {
                     res.redirect('/')
                 })
             } else {
+                req.flash('loginError','Неверный пароль')
                 res.redirect('/auth/login#login')
             }
         } else {
+            req.flash('loginError','Такого пользователя нету')
             res.redirect('/auth/login')
         }
     } catch (e) {
@@ -51,6 +55,7 @@ router.post('/register', async (req, res) => {
         const candidate = await User.findOne({email})
 
         if (candidate) {
+            req.flash('registerError','Пользователь с таким email уже сущетсвует')
             res.redirect('/auth/login#register')
         } else {
             const hashPassword = await bcrypt.hash(password, 10)
