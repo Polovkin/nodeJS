a98c130e21a1d9e8342c7b6dce36fabc1295e905
Polovkin <zimboroda@gmail.com>
2021-01-10 13:02:15 +0200

    56

--- middleware/user.js
+++ middleware/user.js
@@ -4,7 +4,7 @@ module.exports = async function (req, res, next) {
     if (!req.session.user) {
         return next()
     }
-
+    //Добавляем юзера в глобальную область видимости
     req.user = await User.findById(req.session.user._id)
     next()
 }
